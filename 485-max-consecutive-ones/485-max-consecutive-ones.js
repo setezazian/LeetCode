ca14d69b52859c3abc4525270bd290f7677cb026
Shirin Etezazian <64745415+setezazian@users.noreply.github.com>
2022-06-14 18:38:41 -0600

    Time: 115 ms (23.92%), Space: 44.6 MB (59.99%) - LeetHub

--- 485-max-consecutive-ones/485-max-consecutive-ones.js
+++ 485-max-consecutive-ones/485-max-consecutive-ones.js
@@ -4,16 +4,16 @@
  */
 var findMaxConsecutiveOnes = function(nums) {
     var counter = 0;
-    var max = 0;
+    var max1 = 0;
     for (var i = 0; i < nums.length; i++) {
         if (nums[i] === 1) {
             counter++;
-            if (counter > max) {
-                max = counter;
+            if (counter > max1) {
+                max1 = counter;
             }
         } else {
             counter = 0;
         }
     }
-    return max
+    return max1
 };
