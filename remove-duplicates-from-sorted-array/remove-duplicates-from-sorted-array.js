d25966ab0802a4ba61cd0c1ee93bd1c2cd01b796
Shirin Etezazian <64745415+setezazian@users.noreply.github.com>
2022-07-29 16:26:01 -0600

    Time: 104 ms, Memory: 44.7 MB - LeetHub

---
+++ remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    // nums array is sorted
+    // return k = length of the arrray after removing duplicates
+    var k = 1;
+    if (nums.length === 1) {
+        return 1;
+    }
+    for (var i = 1; i < nums.length; i++) {
+        if (nums[i] !== nums[i - 1]) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+    return k;
+};
